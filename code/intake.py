9f4ac7d358fda59844b26d0933fd2e638b8556cc
Michael Haspra <haa@Michaels-MacBook-Pro.local>
2018-11-19 10:58:59 +0100

    Initial commit

---
+++ code/intake.py
@@ -0,0 +1,28 @@
+import json
+import boto3
+import logging
+import os
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+sqs_client = boto3.client('sqs')
+
+def lambda_handler(event, context):
+    logger.info('got event{}'.format(event))
+
+    responseQueueUrl = os.environ['ResponseQueueUrl']
+
+    response = []
+    for message in event['Records']:
+        response.append(
+            sqs_client.send_message(
+                QueueUrl=responseQueueUrl,
+                MessageBody=message['body']
+            )
+        )
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response)
+    }
